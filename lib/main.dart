8576e6f1e20b927777b432d6ee87674a4331d9f2
Iago <iagofalcom@hotmail.com>
2019-06-09 18:37:30 -0300

    Implementação * Função Future<Map> e async, para pegar os dados do request que virão futuramente * Funcão request, que vai receber os dados da API * json.decore, que vai converter os dados para o formato json * Implementação StateFul, para a criação dos códigos * Implementação do Scaffold, para a utilização do layout * Implementção do AppBar, para utilizar a parte superior da Tela e adicionar titulos * Formatação dos itens acima

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http; //biblioteca para acessar requisições web
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert'; //biblioteca para converter strings para json
 
-const request = "";
+const request = "https://api.hgbrasil.com/finance?format=json-cors&key=68a55d29";
 
 void main() async{ //Funcão principal
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
-
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+//Esperando os dados que virão do futuro para criar um mapa json
+Future<Map> getData() async{
+  http.Response response = await http.get(request); //Fazendo uma requisição e esperando os dados chegarem
+  return json.decode(response.body);//Converter os dados que chegaram pelo response para json
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( //Widget que permite usar a barra superior na Tela
+      backgroundColor: Colors.blue,//Cor de fundo da tela
+      appBar: AppBar( //widget barra superior
+        title: Text("Conversor de Moedas"),//Titulo da barra superior
+        backgroundColor: Colors.green, //Cor da barra superior
+        centerTitle: true,//Alinhamento do titulo
+      ),
+    );
+  }
 }
